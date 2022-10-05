0f4fc8d41365102a29611c1159e41b1e43a73629
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-05 15:57:44 -0300

    create class Account and BusinessAccount

---
+++ OOPClassInheritance/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPClassInheritance
+{
+    internal class Account
+    {
+        public Account()
+        {
+        }
+
+        public int Number { get; set; }
+        public string  Holder { get; set; }
+        public double Balance { get; set; }
+
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            amount--;   
+        }
+
+        public void Deposit(double amount)
+        {
+            amount++;
+        }
+
+
+    }
+}
---
+++ OOPClassInheritance/BusinessAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPClassInheritance
+{
+    internal class BusinessAccount : Account
+    {
+        public BusinessAccount()
+        {
+        }
+
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount(double loanLimit)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            amount = amount * LoanLimit;    
+        }
+    }
+}
